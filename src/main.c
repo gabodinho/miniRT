e5c38eb9a96a4086c73a8ffff4a16720745ba9ab
gabodinho <gabodinho80@gmail.com>
2024-08-25 22:20:27 +0200

    deleted some comments from main

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ggiertzu <ggiertzu@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/06 23:25:04 by ggiertzu          #+#    #+#             */
-/*   Updated: 2024/08/25 21:59:25 by ggiertzu         ###   ########.fr       */
+/*   Updated: 2024/08/25 22:19:51 by ggiertzu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,6 @@ void    print_vec(double *v, int size)
     printf("this is a %s: %f, %f, %f\n", str, v[0], v[1], v[2]);
 }
 
-
 void	print_world_info(t_world *w)
 {
 	t_object *obj;
@@ -79,27 +78,7 @@ void	print_world_info(t_world *w)
 	print_vec(w->amb_colour, -1);
 
 }
-/*
-void    test_colour_at(t_world *w, t_camera *c)
-{
-	int		i;
-	int		j;
-	double	*colour;
 
-	i = -1;
-	while (++i < HSIZE)
-	{
-		j = -1;
-		while (++j < VSIZE)
-		{
-			colour = color_at(w, ray_for_pixel(c, i, j));
-			printf("x,y: %d,%d ", i, j);
-			print_vec(colour, -1);
-			free(colour);
-		}
-	}
-}
-*/
 static void	escape(void *ptr)
 {
 	mlx_t	*mlx;
@@ -156,126 +135,3 @@ int	main(int argc, char *argv[])
 	clean_up(w);
 	return (0);
 }
-
-/*
-int	main(void)
-{
-	printf("nobjs: %d\n", get_n_obj("test_file.txt"));
-
-	t_world *w2 = malloc(sizeof(t_world));
-	set_ambient(w2, "0.6    255,255,255 ");
-	// w2 -> amb_colour = point(0.6, 0.6, 0.6);
-	set_light(w2, "-5,5,-10  0.9 ");
-	// w2 -> light_bright = 0.9;
-	// w2 -> light_p = point(-5, 5, -10);
-	w2 -> n_obj = 3;
-	w2 -> objects = malloc(sizeof(t_object *) * w2 -> n_obj);
-	char *str = "-5,-2,0 0.5773,0.5773,-0.5773 50,250,0";
-	printf("%s\n", str);
-	t_object *plane = init_plane(str);
-	w2->objects[0] = plane;
-	char *str2 = "0.0,0.0,10 10 10,0,255";
-	printf("%s\n", str2);
-	t_object *sphere = init_sphere(str2);
-	w2->objects[1] = sphere;
-	char *str3 = "0.0,0.0,10 1,0,0 2 20 255,0,100";
-	printf("%s\n", str3);
-	t_object *cyl = init_cylinder(str3);
-	w2->objects[2] = cyl;
-	printf("plane:\n");
-	print_vec(plane -> norm_v, 0);
-	print_vec(plane -> obj_p, 0);
-	print_vec(plane -> colour, -1);
-	printf("sphere:\n");
-	printf("diam: %f\n", sphere -> diam);
-	print_vec(sphere-> obj_p, 0);
-	print_vec(sphere-> colour, -1);
-	printf("cylinder:\n");
-	printf("diam: %f\n", cyl -> diam);
-	printf("height: %f\n", cyl -> height);
-	print_vec(cyl-> obj_p, 0);
-	print_vec(cyl -> norm_v, 0);
-	print_vec(cyl-> colour, -1);
-	// t_camera *cam;
-    // cam = malloc(sizeof(t_camera));
-	// cam -> nv = vector(0, 0, 1);
-	// cam -> vp = point(0, 0, -5);
-    // cam -> field_of_view = 90;
-    // init_camera(cam);
-	// test_colour_at(w2, cam);
-	char *str4 = "0.0,0,-5 0,0,1 90";
-	t_camera *cam2 = init_camera(str4);
-
-	mlx_t			*mlx;
-	mlx_image_t		*image;
-	mlx = mlx_init(HSIZE, VSIZE, "MLX42", true);
-	image = mlx_new_image(mlx, HSIZE, VSIZE);
-	render(w2, cam2, image);
-	mlx_image_to_window(mlx, image, 0, 0);
-	mlx_loop(mlx);
-	mlx_terminate(mlx);
-    return (0);
-} */
-/*
-int main(void)
-{
-	t_world *w2 = malloc(sizeof(t_world));
-	t_object *obj4 = malloc(sizeof(t_object));
-	obj4 -> shape = SPHERE;
-	obj4 -> transform = translate(-1, 0.5, 0);
-	obj4 -> inv_trans = invert(obj4 -> transform, 4);
-	obj4 -> colour = point(0.5, 0, 0);
-	t_object *obj2 = malloc(sizeof(t_object));
-	obj2 -> shape = PLANE;
-	// keep in mind the order of the transformations!!
-	double *m = rot_x(M_PI / 2);
-	double *m2 = translate(0, 0, 2.99);
-	obj2 -> transform = mat_mat_prod(m2, m, 4);
-	// print_vec(obj2 -> transform, 4);
-	obj2 -> inv_trans = invert(obj2 -> transform, 4);
-	// print_vec(obj2 -> inv_trans, 4);
-	// print_vec(mat_mat_prod(obj2 -> transform, obj2 -> inv_trans, 4), 4);
-	obj2 -> colour = point(0.5, 0.5, 0.5);
-	t_object *obj1 = malloc(sizeof(t_object));
-	obj1 -> shape = PLANE;
-	obj1 -> transform = translate(0, -0.5, 0);
-	obj1 -> inv_trans = invert(obj1 -> transform, 4);
-	obj1 -> colour = point(0, 0.5, 1);
-	t_object *obj3 = malloc(sizeof(t_object));
-	obj3 -> shape = CYLNDR;
-	obj3 -> transform = rot_x(M_PI / 6);
-	obj3 -> transform = scale(1, -2, 1);
-	obj3 -> inv_trans = invert(obj3 -> transform, 4);
-	obj3 -> colour = point(0, 1, 1);
-	w2 -> amb_colour = point(0.6, 0.6, 0.6);
-	w2 -> light_bright = 0.9;
-	w2 -> light_point = point(-5, 5, -10);
-	w2 -> n_obj = 4;
-	w2 -> objects = malloc(sizeof(t_object *) * w2 -> n_obj);
-	w2->objects[0] = obj4;
-	w2->objects[1] = obj2;
-	w2->objects[2] = obj1;
-	w2->objects[3] = obj3;
-	t_camera *cam;
-    cam = malloc(sizeof(t_camera));
-    cam -> vp_x = 0;
-    cam -> vp_y = 0;
-    cam -> vp_z = -3;
-    cam -> nv_x = 0;
-    cam -> nv_y = 0;
-    cam -> nv_z = 1;
-    cam -> field_of_view = 90;
-    init_camera(cam);
-	// test_colour_at(w2, cam);
-
-	mlx_t			*mlx;
-	mlx_image_t		*image;
-	mlx = mlx_init(HSIZE, VSIZE, "MLX42", true);
-	image = mlx_new_image(mlx, HSIZE, VSIZE);
-	render(w2, cam, image);
-	mlx_image_to_window(mlx, image, 0, 0);
-	mlx_loop(mlx);
-	mlx_terminate(mlx);
-    return (0);
-}
- */
